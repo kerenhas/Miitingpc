1c2b68994c6dc36db1b47f8ebac77c3c091a4219
kerenhas <48164083+kerenhas@users.noreply.github.com>
2020-09-16 12:10:48 +0200

    Update v_carousel_entreprises.php

--- vues/v_carousel_entreprises.php
+++ vues/v_carousel_entreprises.php
@@ -52,7 +52,7 @@
                          <img class="card-img-top" src="<?php echo $carspreta[$i]['logo'];?>" alt="Card image cap">
                             <div class="card-body">
                                 <h4 class="card-title"><?php echo $carspreta[$i]['libelle'];?></h4>
-                                <p class="card-text"><?php  echo $carspreta[$i]['description'];?> </p>        
+                                <p class="card-text"><?php echo fonctionsUtils::tronque($carspreta[$i]['description'], 80); ?></p>        
                             </div>
                         </div>
                          <?php
